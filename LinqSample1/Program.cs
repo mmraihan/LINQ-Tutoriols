6a1dcfd16945112d3758f2b21469ad024e5cf772
mmraihan <mm_raihan@live.com>
2021-07-04 20:44:45 +0600

    Select in LINQ: Select inly ID and Email

--- LinqSample1/Program.cs
+++ LinqSample1/Program.cs
@@ -18,48 +18,16 @@ namespace LinqSample1
                 new Employee(){Id=5, Name= "Raihan5", Email="admin@exaple.com"}
             };
 
+            var selectQuery = (from emp in employees
+                              select new Employee()
+                              {
+                                  Id=emp.Id,
+                                  Email=emp.Email
+                              }).ToList();
 
-            /////////////////////////////1. Basic Query/////////////////////////
-            var basicQuery = (from emp in employees
-                             select emp).ToList();
-
-            foreach (var item in basicQuery)
+            foreach (var item in selectQuery)
             {
-                Console.WriteLine ($"Id={item.Id}, Name={item.Name}, Email= {item.Email}" );
-            }
-
-            Console.WriteLine("---------------------------------------------------------");
-
-            /////////////////////////////2. Basic Method/////////////////////////
-
-            var basicMethod = employees.ToList();
-            foreach (var item in basicMethod)
-            {
-                Console.WriteLine($"Basic Method:  Id={item.Id}, Name={item.Name}, Email= {item.Email}");
-
-            }
-            Console.WriteLine("---------------------------------------------------------");
-
-
-            /////////////////////////////////////Operations///////////////////////
-            ///
-            var basicPropQuery = (from emp in employees
-                                  select emp.Id).ToList();
-
-            foreach (var item in basicPropQuery)
-            {
-                Console.WriteLine($"Id={item}");
-
-            }
-
-            Console.WriteLine("---------------------------------------------------------");
-
-
-            var basicPropMethod = employees.Select(emp => emp.Id).ToList();
-
-            foreach (var item in basicPropMethod)
-            {
-                Console.WriteLine($"Id={item}");
+                Console.WriteLine($"Id={item.Id}, Email={item.Email}");
 
             }
 
