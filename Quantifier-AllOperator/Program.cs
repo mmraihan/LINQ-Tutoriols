02bca499e1d18e718c89c3144fab0e6313f0122e
mmraihan <mm_raihan@live.com>
2021-10-11 15:36:19 +0600

    Quantifier-1 - All Operator

---
+++ Quantifier-AllOperator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quantifier_AllOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var datasource = new List<Student>()
+            {
+                new Student(){Name="raihan", Marks=90,Subjects= new List<Subject>()
+                {
+                 new Subject(){SubName="C#", SunMarks=75},
+                 new Subject(){SubName="C#2", SunMarks=85},
+                 new Subject(){SubName="C#3", SunMarks=80},
+                 new Subject(){SubName="C#4", SunMarks=75},
+                }
+                },
+
+                new Student(){Name="Mridul", Marks=85,Subjects= new List<Subject>()
+                {
+                 new Subject(){SubName="C#", SunMarks=89},
+                 new Subject(){SubName="C#2", SunMarks=85},
+                 new Subject(){SubName="C#3", SunMarks=90},
+                 new Subject(){SubName="C#4", SunMarks=95},
+                }
+                },
+
+                new Student(){Name="Taneeb", Marks=75,Subjects= new List<Subject>()
+                {
+                 new Subject(){SubName="C#", SunMarks=75},
+                 new Subject(){SubName="C#2", SunMarks=85},
+                 new Subject(){SubName="C#3", SunMarks=80},
+                 new Subject(){SubName="C#4", SunMarks=65},
+                }
+                }
+               
+            };
+
+            var ms = datasource.Where(std => std.Subjects.All(x => x.SunMarks > 70)).Select(std2 => std2).ToList();
+
+            foreach (var item in ms)
+            {
+                Console.WriteLine(item.Name);
+           
+            }
+          
+           
+
+
+          
+        }
+    }
+}
---
+++ Quantifier-AllOperator/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantifier_AllOperator
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+
+        public List<Subject> Subjects { get; set; }
+
+    }
+
+    public class Subject
+    {
+        public string SubName { get; set; }
+        public int SunMarks { get; set; }
+    }
+}
